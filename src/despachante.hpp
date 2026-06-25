ccdb5bbaaf17ee310081766d2fdb942c46bd5d38
Gabriel Francisco de Oliveira Castro <gabriel.alcantara.castro@gmail.com>
2026-06-25 10:26:56 -0300

    Refatorando

--- src/despachante.hpp
+++ src/despachante.hpp
@@ -25,9 +25,9 @@ class Despachante {
         void executar(){
             int tempo_atual = 0;
 
-            while(proximo_a_ser_criado < (int)todos_os_processos.size() || !filas.todas_as_filas_estao_vazias()){
+            while(proximo_processo_a_entrar_nas_filas < (int)todos_os_processos.size() || !filas.todas_as_filas_estao_vazias()){
 
-                admitir_processos(tempo_atual);
+                atualizar_filas_com_novos_processos(tempo_atual);
 
                 if (filas.todas_as_filas_estao_vazias()){
                     tempo_atual++;
@@ -46,7 +46,7 @@ class Despachante {
     private:
         vector<BCP> todos_os_processos;
         GerenciadorDeFilas filas;
-        int proximo_a_ser_criado = 0;
+        int proximo_processo_a_entrar_nas_filas = 0;
 
         // 1) Abre o arquivo em modo leitura.
         void carregar_processos(const string& nome_arquivo){
@@ -67,10 +67,11 @@ class Despachante {
             if (!arquivo.is_open()){
                 throw runtime_error("Não foi possível abrir: " + nome_arquivo);
             }
-
+            // Buffer temporário que vai receber cada linha do arquivo
             string linha;
             int idx = 0;
             while (getline(arquivo, linha) && idx < (int)todos_os_processos.size()){
+                // Ignora linhas vazias
                 if (linha.find_first_not_of(" \t\r\n") == string::npos){
                     continue;
                 }
@@ -105,10 +106,12 @@ class Despachante {
             }
         }
 
-        void admitir_processos(int tempo_atual){
-            while (proximo_a_ser_criado < (int)todos_os_processos.size() && todos_os_processos[proximo_a_ser_criado].tempo_chegada <= tempo_atual){
-                filas.adicionar_processos_na_fila(todos_os_processos[proximo_a_ser_criado]);
-                proximo_a_ser_criado++;
+        // "Libera" a entrada dos processos na fila quando a variável tempo_atual for igual ao tempo_de_chegada
+        void atualizar_filas_com_novos_processos(int tempo_atual){
+            // Lidar com os vários processos que chegam simultaneamente
+            while (proximo_processo_a_entrar_nas_filas < (int)todos_os_processos.size() && todos_os_processos[proximo_processo_a_entrar_nas_filas].tempo_chegada <= tempo_atual){
+                filas.adicionar_processos_na_fila(todos_os_processos[proximo_processo_a_entrar_nas_filas]);
+                proximo_processo_a_entrar_nas_filas++;
             }
         }
 
@@ -143,7 +146,7 @@ class Despachante {
                 int instrucao = processo.tempo_processador_necessario - processo.tempo_processador_restante;
                 cout << "P" << processo.id << " instruction " << instrucao << "\n";
                 tempo_atual++;
-                admitir_processos(tempo_atual);
+                atualizar_filas_com_novos_processos(tempo_atual);
             }
             finalizar_processo(processo);
         }
@@ -154,7 +157,7 @@ class Despachante {
             int instrucao = processo.tempo_processador_necessario - processo.tempo_processador_restante;
             cout << "P" << processo.id << " instruction " << instrucao << "\n";
             tempo_atual++;
-            admitir_processos(tempo_atual);
+            atualizar_filas_com_novos_processos(tempo_atual);
 
             if (processo.tempo_processador_restante <= 0){
                 finalizar_processo(processo);
